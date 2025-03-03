cbe21e2dd1c5e1287a71eb8ef2b6a126472c31aa
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-03-03 11:55:32 +0500

    register

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,10 +1,15 @@
+import { verify } from "crypto";
 import UserSchema from "../schema/userschema.js";
+import { signInJwt } from "../utils/jwt.js";
+import { ResData } from "../utils/response-data.js";
 
 export const signUp = async (req, res, next) => {
   try {
     const body = req.body;
-    await UserSchema.create({ ...body });
-    res.send("ok");
+    const data = await UserSchema.create({ ...body });
+    const token = signInJwt({ id: data._id });
+    const resData = new ResData(201, "succsess", { user: data, token });
+    res.status(resData.status).json(resData);
   } catch (error) {
     next(error);
   }
